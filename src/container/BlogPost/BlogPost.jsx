cbe9039429e0563485a099aa09358c06c7ea9e48
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-04-03 19:54:33 +0700

    Api get with axios

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,13 +1,42 @@
 import React, { Fragment } from 'react';
+import axios from 'axios';
 import Post from '../../component/Post/Post';
 import './BlogPost.css';
 
 class BlogPost extends React.Component {
+	state = {
+		post: [],
+	};
+
+	componentDidMount() {
+		// fetch('https://jsonplaceholder.typicode.com/posts')
+		// 	.then((response) => response.json())
+		// 	.then((json) => {
+		// 		this.setState({
+		// 			post: json,
+		// 		});
+		// 	});
+
+		axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+			this.setState({
+				post: res.data,
+			}).catch((err) => console.log(err));
+		});
+	}
+
 	render() {
 		return (
 			<Fragment>
 				<p className="section-title">Blog Post</p>
-				<Post title="title" desc="desc" />
+				{this.state.post.map((post) => {
+					return (
+						<Post
+							title={post.title}
+							desc={post.body}
+							key={post.id}
+						/>
+					);
+				})}
 			</Fragment>
 		);
 	}
